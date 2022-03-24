a2fae752922569362a73a66863d7fbab99913111
David Sheffield <sheffield.david@gmail.com>
2022-03-24 07:32:52 -0700

    report cycles per buslock

--- buslock.cc
+++ buslock.cc
@@ -17,56 +17,80 @@ using namespace std;
 static int thr_online;
 static int *lock = nullptr;
 
-double timestamp() {
+static uint64_t *cpu_cycles = nullptr;
+
+static inline double timestamp() {
   struct timeval tv;
   gettimeofday(&tv, nullptr);
   return tv.tv_sec + static_cast<double>(tv.tv_usec)*1e-6;
 }
 
+static inline uint64_t read_counter() {
+  uint32_t hi=0, lo=0;
+  __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+  return static_cast<uint64_t>(lo)|(static_cast<uint64_t>(hi)<<32);
+}
+
 void thr_func(int tid) {
-  bool success = false;
+  int *ptr = lock;
+  uint64_t start;
   //spin barrier
   __sync_fetch_and_sub(&thr_online, 1);
   while(*reinterpret_cast<volatile int*>(&thr_online)) {
     __builtin_ia32_pause();
   }
-  for(int i = 0; i < iters; i++) {
-    do {
-      int old = *lock;
-      success = __sync_bool_compare_and_swap(lock, old, tid);
-    }
-    while(!success);
-    __builtin_ia32_pause();
+  start = read_counter();
+  for(int i = 0; i < iters; i+=16) {
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);     
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);
+     __sync_fetch_and_add(ptr, 1);     
   }
+  cpu_cycles[tid] = (read_counter() - start)/iters;
 }
 
 int main() {
   vector<thread> threads;
-  int n_thr = sysconf(_SC_NPROCESSORS_ONLN);
+  int max_thr = sysconf(_SC_NPROCESSORS_ONLN);
   int cl_len = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+  cpu_cycles = new uint64_t[max_thr];
   
   void *ptr = mmap(nullptr, 4096, PROT, MAP, -1, 0);
 
   std::cout << "OS believes cacheline length is " << cl_len << " bytes\n";
-  
-  for(int disp = cl_len-sizeof(int); disp < cl_len+sizeof(int); ++disp) {
-    lock = reinterpret_cast<int*>(reinterpret_cast<char*>(ptr) + disp);
-    *lock = n_thr+1;
-    thr_online = n_thr;
-    double now = timestamp();
-    for(int thr = 0; thr < n_thr; ++thr) {
-      threads.push_back(thread(thr_func, thr));
-    }
-    
-    for(thread &thr : threads) {
-      if(thr.joinable()) {
-	thr.join();
+
+  for(int n_thr = 1; n_thr <= max_thr; ++n_thr) {
+    std::cout << "running with " << n_thr << " threads\n";
+    for(int disp = cl_len-sizeof(int); disp < cl_len+sizeof(int); ++disp) {
+      lock = reinterpret_cast<int*>(reinterpret_cast<char*>(ptr) + disp);
+      *lock = n_thr+1;
+      thr_online = n_thr;
+      for(int thr = 0; thr < n_thr; ++thr) {
+	threads.push_back(thread(thr_func, thr));
       }
+      
+      for(thread &thr : threads) {
+	if(thr.joinable()) {
+	  thr.join();
+	}
+      }
+      std::cout << "disp = " << disp << ", cycles = " << cpu_cycles[0] << "\n";
+      threads.clear();
     }
-    now = timestamp() - now;
-    std::cout << "disp = " << disp << ", now = " << now << "\n";
-    threads.clear();
   }
   munmap(ptr, 4096);
+  delete [] cpu_cycles;
   return 0;
 }
